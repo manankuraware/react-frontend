4ba73b82ab1eafb9cad663ada5e361a286d5c66c
manankuraware <manankuraware2017@gmail.com>
2023-11-01 18:22:20 +0530

    roger that

--- Hooks/useStateHook/src/App.jsx
+++ Hooks/useStateHook/src/App.jsx
@@ -1,33 +1,45 @@
-import { useState } from 'react'
-import './App.css'
+import React, { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(4);
+  // const [count, setCount] = useState(()=>{
+  //   console.log('run function');
+  //   // here run function prints only once
+  //   return 4;
+  // });
 
+  // function decrementCount() {
+  //   // setCount(count - 1);
+  //   // setCount(prevCount=>prevCount-1);
+  //   setCount(prevCount=>prevCount-1);
+  // }
+  // function incrementCount() {
+  //   setCount(prevCount=>prevCount+1);
+  // }
+
+  //passing object as parameter
+  const [state, setState] = useState({ count: 4, theme: "blue" });
+  const count = state.count;
+  const theme = state.theme;
+
+  function decrementCount() {
+    setState((prevState) => {
+      return { ...prevState, count: prevState.count - 1 };
+    });
+  }
+  function incrementCount() {
+    setState((prevState) => {
+      return { ...prevState, count: prevState.count + 1 };
+    });
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={decrementCount}>-</button>
+      <span>{count}</span>
+      <span>{theme}</span>
+      <button onClick={incrementCount}>+</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
