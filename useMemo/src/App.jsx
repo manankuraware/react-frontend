78a58e10a8ddf849b4699e2e596324fb9f18449f
manankuraware <manankuraware2017@gmail.com>
2023-11-06 11:32:47 +0530

    first commit

--- useMemo/src/App.jsx
+++ useMemo/src/App.jsx
@@ -1,5 +1,34 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 
-function App() {}
+function App() {
+    const [number, setNumber] = useState(0);
+    const [dark, setDark] = useState(false);
+    const doubleNumber = useMemo(() => {
+        return slowFunction(number);
+    }, [number]);
+    const themeStyles = {
+        backgroundColor: dark ? "black" : "white",
+        color: dark ? "white" : "black",
+    };
 
+    return (
+        <>
+            <input
+                type="number"
+                value={number}
+                onChange={(e) => setNumber(parseInt(e.target.value))}
+            />
+            <button onClick={() => setDark((prevDark) => !prevDark)}>
+                Change Theme
+            </button>
+            <div style={themeStyles}>{doubleNumber}</div>
+        </>
+    );
+}
+
+function slowFunction(num) {
+    console.log("calling slow function");
+    for (let i = 0; i <= 1000000000; i++) { }
+    return num * 2;
+}
 export default App;
