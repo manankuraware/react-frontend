a3cb77e33e971cdaf36c0e89c5a43c07870c0901
manankuraware <manankuraware2017@gmail.com>
2023-11-08 14:05:57 +0530

    first commit

--- useLayoutEffect/src/App.jsx
+++ useLayoutEffect/src/App.jsx
@@ -1,43 +1,25 @@
-import React, { useState } from "react";
+import React, {useLayoutEffect, useRef, useState } from "react";
 
 function App() {
-  // const [count, setCount] = useState(4);
-  // const [count, setCount] = useState(()=>{
-  //   console.log('run function');
-  //   // here run function prints only once
-  //   return 4;
-  // });
+  const [show, setShow] = useState(false);
+  const popup = useRef();
+  const button = useRef();
+  useLayoutEffect(() => {
+    if (popup.current == null || button.current == null) return;
+    const { bottom } = button.current.getBoundingClientRect();
+    popup.current.style.top = `${bottom + 25}px`;
+  }, [show]);
 
-  // function decrementCount() {
-  //   // setCount(count - 1);
-  //   // setCount(prevCount=>prevCount-1);
-  //   setCount(prevCount=>prevCount-1);
-  // }
-  // function incrementCount() {
-  //   setCount(prevCount=>prevCount+1);
-  // }
-
-  //passing object as parameter
-  const [state, setState] = useState({ count: 4, theme: "blue" });
-  const count = state.count;
-  const theme = state.theme;
-
-  function decrementCount() {
-    setState((prevState) => {
-      return { ...prevState, count: prevState.count - 1 };
-    });
-  }
-  function incrementCount() {
-    setState((prevState) => {
-      return { ...prevState, count: prevState.count + 1 };
-    });
-  }
   return (
     <>
-      <button onClick={decrementCount}>-</button>
-      <span>{count}</span>
-      <span>{theme}</span>
-      <button onClick={incrementCount}>+</button>
+      <button ref={button} onClick={() => setShow((prev) => !prev)}>
+        Click Here
+      </button>
+      {show && (
+        <div style={{ position: "absolute" }} ref={popup}>
+          This is a popup
+        </div>
+      )}
     </>
   );
 }
